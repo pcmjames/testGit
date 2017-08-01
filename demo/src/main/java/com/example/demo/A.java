6a773c24c8a7f0f2029578ae6f3a0a90dce42578
pcmjames <james.peng@connext.com.cn>
2017-08-01 10:55:00 +0800

    新增A类

---
+++ demo/src/main/java/com/example/demo/A.java
@@ -0,0 +1,14 @@
+/**
+ * Created by James Peng on 2017/8/1.
+ */
+
+package com.example.demo;
+
+public class A
+{
+
+  public static void main(String[] args)
+  {
+    System.out.println("A1");
+  }
+}
