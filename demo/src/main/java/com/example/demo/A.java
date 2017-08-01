4d3fde2d054efd7a43e4b02422844ccdb2fbfedd
pcmjames <james.peng@connext.com.cn>
2017-08-01 11:30:27 +0800

    新增一行代码

--- demo/src/main/java/com/example/demo/A.java
+++ demo/src/main/java/com/example/demo/A.java
@@ -10,6 +10,7 @@ public class A
   public static void main(String[] args)
   {
     System.out.println("A from pcm_future_2");
+    System.out.println("=============================");
     System.out.println("A1");
   }
 }
