06d3aa02c9186599dc212f0e9ab1721c90a44762
pcmjames <james.peng@connext.com.cn>
2017-08-01 11:21:27 +0800

    attempt1

--- demo/src/main/java/com/example/demo/A.java
+++ demo/src/main/java/com/example/demo/A.java
@@ -9,10 +9,7 @@ public class A
 
   public static void main(String[] args)
   {
-<<<<<<< HEAD
     System.out.println("A from pcm_future_2");
-=======
     System.out.println("A1");
->>>>>>> 6a773c24c8a7f0f2029578ae6f3a0a90dce42578
   }
 }
