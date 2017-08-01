08ff9796c3de387adc3213e9d43b2b074b4e44fa
pcmjames <james.peng@connext.com.cn>
2017-08-01 11:10:43 +0800

    新增A2类

---
+++ demo/src/main/java/com/example/demo/A2.java
@@ -0,0 +1,14 @@
+/**
+ * Created by James Peng on 2017/8/1.
+ */
+
+package com.example.demo;
+
+public class A2
+{
+
+  public static void main(String[] args)
+  {
+    System.out.println("A2");
+  }
+}
